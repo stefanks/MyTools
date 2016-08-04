fc51b605e92a325b1749935833946762e6b8fc21
Stefan S <solntsev@wisc.edu>
2016-08-04 15:20:09 -0500

    working version

--- RawLockMass/Program.cs
+++ RawLockMass/Program.cs
@@ -17,62 +17,61 @@ namespace RawLockMass
 
             var Compound1 = new Peptide("NNNNN");
 
-            var dist1 = new IsotopicDistribution(Compound1.GetChemicalFormula(), 1, 0.001);
-            Console.WriteLine("NNNNN masses: " + string.Join(", ", dist1.Masses));
-            var dist1MZ = dist1.Masses.Select(b => b.ToMassToChargeRatio(1));
+            var dist1MZ = new IsotopicDistribution(Compound1.GetChemicalFormula(), 1, 0.001).Masses.Select(b => b.ToMassToChargeRatio(1)).ToList();
             Console.WriteLine("NNNNN mz: " + string.Join(", ", dist1MZ));
 
             var chemFormulaOfCompound2 = Compound1.GetChemicalFormula();
             chemFormulaOfCompound2.Add(new ChemicalFormula("N-1H-2"));
-            var dist2 = new IsotopicDistribution(chemFormulaOfCompound2, 1, 0.001);
-            Console.WriteLine("Compound2 masses: " + string.Join(", ", dist2.Masses));
-            var dist2MZ = dist2.Masses.Select(b => b.ToMassToChargeRatio(1));
+            var dist2MZ = new IsotopicDistribution(chemFormulaOfCompound2, 1, 0.001).Masses.Select(b => b.ToMassToChargeRatio(1)).ToList();
             Console.WriteLine("Compound2 mz: " + string.Join(", ", dist2MZ));
 
 
-            using (System.IO.StreamWriter shiftsFile = new System.IO.StreamWriter(@"shifts.txt"))
+            foreach (var arg in args)
             {
-                foreach (var arg in args)
+                using (System.IO.StreamWriter shiftsFile = new System.IO.StreamWriter(arg+".tsv"))
                 {
                     var file = new ThermoRawFile(arg);
                     file.Open();
 
                     shiftsFile.WriteLine(file.FilePath);
-                    shiftsFile.WriteLine(1);
                     Console.WriteLine(file.FilePath);
                     Console.WriteLine(file.FileType);
 
-                    //foreach (var a in file.GetScan(911).MassSpectrum)
-                    //{
-                    //    Console.WriteLine(a);
-                    //    if (a.MZ > 600)
-                    //        Console.WriteLine("h");
-                    //}
-
-
                     foreach (var scan in file)
                     {
-                        Console.WriteLine(" " + scan);
-
-                        List<MzPeak> theoreticalPeaksForCompound1;
-                        List<MzPeak> theoreticalPeaksForCompound2;
-                        List<MzPeak> experimentalPeaksForCompound1;
-                        List<MzPeak> experimentalPeaksForCompound2;
-
-                        if (scan.ScanNumber == 911)
+                        double monoError = double.NaN;
+                        double sumErrors = double.NaN;
+                        var extract1 = scan.MassSpectrum.newSpectrumExtract(dist1MZ[0] - 0.004, dist1MZ[0] + 0.004);
+                        int count = 0;
+                        if (extract1.Count > 0)
                         {
-                            Console.WriteLine("okay...");
+                            monoError = dist1MZ[0] - extract1.PeakWithHighestY.MZ;
+                            count += 1;
+
+                            // Let's look for other isotopic peaks!
+                            for (int i = 1; i < 4; i++)
+                            {
+                                var extract = scan.MassSpectrum.newSpectrumExtract(dist1MZ[i] - monoError - 0.0004, dist1MZ[i] - monoError + 0.0004);
+                                if (extract.Count > 0)
+                                {
+                                    if (double.IsNaN(sumErrors))
+                                        sumErrors = monoError;
+                                    sumErrors += dist1MZ[i] - extract.PeakWithHighestY.MZ;
+                                    count += 1;
+                                }
+                            }
+                            sumErrors /= count;
                         }
 
-                        var extract = scan.MassSpectrum.newSpectrumExtract(dist1MZ.First() - 0.004, dist1MZ.First() + 0.004);
-
-
-                        //Console.WriteLine(" Error of most intense peak: " +);
-                        //Console.WriteLine(" Simple average of all matched peaks: " +);
-                        //Console.WriteLine(" Simple average of most intense peak: " +);
-                        //Console.WriteLine(" Simple average of all matched peaks: " +);
-                        //Console.WriteLine(" Simple average of all matched peaks: "  +);
-                        //Console.WriteLine(" Simple average of all matched peaks: " +);
+                        if (count > 1)
+                        {
+                            Console.WriteLine("scan " + scan.ScanNumber);
+                            Console.WriteLine("count = " + count);
+                            Console.WriteLine("monoError = " + monoError);
+                            Console.WriteLine("sumErrors = " + sumErrors);
+                        }
+                        shiftsFile.WriteLine(scan.ScanNumber + "\t" + monoError+"\t"+ sumErrors);
+                        
                     }
                 }
             }
