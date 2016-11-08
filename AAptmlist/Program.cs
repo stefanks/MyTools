c25be821f030065eba1144fa9a546c9aef83175b
Stefan S <solntsev@wisc.edu>
2016-11-08 09:43:01 -0600

    okay

---
+++ AAptmlist/Program.cs
@@ -0,0 +1,86 @@
+﻿using Proteomics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsefulProteomicsDatabases;
+
+namespace AAptmlist
+{
+    class Program
+    {
+        static double tol = 0.0001;
+        static void Main(string[] args)
+        {
+            Loaders.LoadElements("ok.txt");
+            List<char> aminoAcidsToConsider = new List<char>() { 'G', 'A', 'S', 'P', 'V', 'T', 'C', 'I', 'L', 'N', 'D', 'Q', 'K', 'E', 'M', 'H', 'F', 'R', 'Y', 'W' };
+            List<Tuple<AminoAcid, AminoAcid, double>> tuples = new List<Tuple<AminoAcid, AminoAcid, double>>();
+            foreach (var ok in aminoAcidsToConsider)
+            {
+                var aa1 = AminoAcid.GetResidue(ok);
+                foreach (var ok2 in aminoAcidsToConsider)
+                {
+                    var aa2 = AminoAcid.GetResidue(ok2);
+
+                    var diff = aa2.MonoisotopicMass - aa1.MonoisotopicMass;
+
+                    var tuple = new Tuple<AminoAcid, AminoAcid, double>(aa1, aa2, diff);
+                    tuples.Add(tuple);
+                }
+            }
+
+            // IGNORE ALL ZERO MASSES AND SUBSTITUTIONS TO ISOLEUCINE
+            tuples = tuples.OrderBy(b => b.Item3).Where(b => b.Item3 != 0).ToList();
+
+            Console.WriteLine("FOR EXCEL");
+
+            double prevDiff = double.MinValue;
+            foreach (var ok in tuples)
+            {
+                if (prevDiff > ok.Item3 - tol && prevDiff < ok.Item3 + tol)
+                    Console.Write(" or " + ok.Item1.Name + "->" + ok.Item2.Name);
+                else
+                {
+                    Console.Write("\n" + ok.Item3 + ";" + ok.Item1.Name + "->" + ok.Item2.Name);
+                    prevDiff = ok.Item3;
+                }
+            }
+
+            Console.WriteLine("\n\nFOR PTMLIST");
+            foreach (var ok in tuples)
+            {
+                Console.WriteLine("ID   " + ok.Item1.Name + "->" + ok.Item2.Name);
+                Console.WriteLine("FT   MOD_RES");
+                Console.WriteLine("TG   " + ok.Item1.Name);
+                Console.WriteLine("PP   Anywhere");
+                Console.WriteLine("MM   " + ok.Item3);
+                Console.WriteLine("//");
+            }
+
+            Console.WriteLine("\n\nFOR EXCEL");
+
+            aminoAcidsToConsider.Reverse();
+
+            foreach (var ok in aminoAcidsToConsider)
+            {
+                var aa1 = AminoAcid.GetResidue(ok);
+                Console.WriteLine(-aa1.MonoisotopicMass + ";" + aa1.Name + " not here");
+            }
+
+            Console.WriteLine("\n\nFOR PTMLIST");
+            foreach (var ok in aminoAcidsToConsider)
+            {
+                var aa1 = AminoAcid.GetResidue(ok);
+                Console.WriteLine("ID   " + aa1.Name + " not here");
+                Console.WriteLine("FT   MOD_RES");
+                Console.WriteLine("TG   " + aa1.Name);
+                Console.WriteLine("PP   Anywhere");
+                Console.WriteLine("MM   -" + aa1.MonoisotopicMass);
+                Console.WriteLine("//");
+            }
+
+            Console.Read();
+        }
+    }
+}
--- RawLockMass/Program.cs
+++ RawLockMass/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Chemistry;
 using IO.Thermo;
 using Proteomics;
-using Spectra;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,65 +12,49 @@ namespace RawLockMass
         static void Main(string[] args)
         {
             UsefulProteomicsDatabases.Loaders.LoadElements("elements.dat");
-
+            var tol = 0.004;
 
             var Compound1 = new Peptide("NNNNN");
 
-            var dist1MZ = new IsotopicDistribution(Compound1.GetChemicalFormula(), 1, 0.001).Masses.Select(b => b.ToMassToChargeRatio(1)).ToList();
-            Console.WriteLine("NNNNN mz: " + string.Join(", ", dist1MZ));
+            var regularMZ = new IsotopicDistribution(Compound1.GetChemicalFormula(), 0.1, 0.001).Masses.Select(b => b.ToMassToChargeRatio(1)).ToList();
+            Console.WriteLine("NNNNN mz: " + string.Join(", ", regularMZ));
 
-            //var chemFormulaOfCompound2 = Compound1.GetChemicalFormula();
-            //chemFormulaOfCompound2.Add(new ChemicalFormula("N-1H-2"));
-            //var dist2MZ = new IsotopicDistribution(chemFormulaOfCompound2, 1, 0.001).Masses.Select(b => b.ToMassToChargeRatio(1)).ToList();
-            //Console.WriteLine("Compound2 mz: " + string.Join(", ", dist2MZ));
+            var withAmmoniaLoss = Compound1.GetChemicalFormula();
+            withAmmoniaLoss.Add(new ChemicalFormula("N-1H-2"));
+            var withAmmoniaLossMZ = new IsotopicDistribution(withAmmoniaLoss, 0.1, 0.001).Masses.Select(b => b.ToMassToChargeRatio(1)).ToList();
+            Console.WriteLine("withAmmoniaLoss mz: " + string.Join(", ", withAmmoniaLossMZ));
 
+            var deamidated = Compound1.GetChemicalFormula();
+            deamidated.Add(new ChemicalFormula("H-1N-1O"));
+            var deamidatedMZ = new IsotopicDistribution(deamidated, 0.1, 0.001).Masses.Select(b => b.ToMassToChargeRatio(1)).ToList();
+            Console.WriteLine("deamidated mz: " + string.Join(", ", deamidatedMZ));
+
+            List<List<double>> allDistributions = new List<List<double>>() { regularMZ, withAmmoniaLossMZ, deamidatedMZ };
 
             foreach (var arg in args)
             {
-                using (System.IO.StreamWriter shiftsFile = new System.IO.StreamWriter(arg+".tsv"))
+                using (System.IO.StreamWriter shiftsFile = new System.IO.StreamWriter(arg + ".tsv"))
                 {
                     var file = new ThermoRawFile(arg);
                     file.Open();
 
                     shiftsFile.WriteLine(file.FilePath);
                     Console.WriteLine(file.FilePath);
-                    Console.WriteLine(file.FileType);
 
                     foreach (var scan in file)
                     {
+                        double bestIntensity = 0;
                         double monoError = double.NaN;
-                        double sumErrors = double.NaN;
-                        var extract1 = scan.MassSpectrum.newSpectrumExtract(dist1MZ[0] - 0.004, dist1MZ[0] + 0.004);
-                        int count = 0;
-                        if (extract1.Count > 0)
+                        foreach (var dist in allDistributions)
                         {
-                            monoError = dist1MZ[0] - extract1.PeakWithHighestY.MZ;
-                            count += 1;
-
-                            // Let's look for other isotopic peaks!
-                            for (int i = 1; i < 4; i++)
+                            var monoisotopicPeak = scan.MassSpectrum.newSpectrumExtract(dist[0] - tol, dist[0] + tol).PeakWithHighestY;
+                            if (monoisotopicPeak != null && bestIntensity < monoisotopicPeak.Intensity)
                             {
-                                var extract = scan.MassSpectrum.newSpectrumExtract(dist1MZ[i] - monoError - 0.0004, dist1MZ[i] - monoError + 0.0004);
-                                if (extract.Count > 0)
-                                {
-                                    if (double.IsNaN(sumErrors))
-                                        sumErrors = monoError;
-                                    sumErrors += dist1MZ[i] - extract.PeakWithHighestY.MZ;
-                                    count += 1;
-                                }
+                                bestIntensity = monoisotopicPeak.Intensity;
+                                monoError = dist[0] - monoisotopicPeak.MZ;
                             }
-                            sumErrors /= count;
                         }
-
-                        if (count > 1)
-                        {
-                            Console.WriteLine("scan " + scan.ScanNumber);
-                            Console.WriteLine("count = " + count);
-                            Console.WriteLine("monoError = " + monoError);
-                            Console.WriteLine("sumErrors = " + sumErrors);
-                        }
-                        shiftsFile.WriteLine(scan.ScanNumber + "\t" + monoError+"\t"+ sumErrors);
-                        
+                        shiftsFile.WriteLine(scan.ScanNumber + "\t" + monoError);
                     }
                 }
             }
